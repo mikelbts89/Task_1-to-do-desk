b5daa555ddf52067a803bbedd92859b035de0e14
mikelbts89 <mikelbts89@gmail.com>
2020-11-12 22:09:59 +0200

    adding_some_new_js_code

--- app.js
+++ app.js
@@ -1,28 +1,37 @@
-let tasksDiv = document.getElementById("tasks_div");
-let textInp = document.getElementById("text_inp");
+let tasks_div = document.getElementById("tasks_div");
+let text_inp = document.getElementById("text_inp");
+let date_inp = document.getElementById("date_inp");
 
 const createTask = () => {
-  let mainDiv = document.createElement("div");
+  console.log(date_inp.value);
+  let main_div = document.createElement("div");
   let paragraph = document.createElement("div");
+  let date_span = document.createElement("span");
+  date_span.innerText = date_inp.value;
   paragraph.className = "main_parag";
-  mainDiv.className = "main_div";
-  let textValue = textInp.value;
-  paragraph.innerText = textValue;
-  mainDiv.appendChild(paragraph);
-  tasksDiv.appendChild(mainDiv);
+  main_div.className = "main_div";
+  let text_value = text_inp.value;
+  if (!text_value.trim()) {
+    return alert("No Task !");
+  }
+  paragraph.innerText = text_value;
+  main_div.appendChild(paragraph);
+  main_div.appendChild(date_span);
+  tasks_div.appendChild(main_div);
 
-  mainDiv.addEventListener("mouseenter", () => {
-    let deleteBtn = document.createElement("button");
-    deleteBtn.className = "delete_btn";
-    deleteBtn.innerText = "X";
-    mainDiv.appendChild(deleteBtn);
-    deleteBtn.addEventListener("click", (e) => {
+  main_div.addEventListener("mouseenter", () => {
+    let delete_btn = document.createElement("button");
+    delete_btn.className = "delete_btn";
+    delete_btn.innerText = "X";
+    main_div.appendChild(delete_btn);
+    delete_btn.addEventListener("click", (e) => {
       e.target.parentElement.remove();
     });
-    mainDiv.addEventListener("mouseleave", () => {
-      deleteBtn.remove();
+    main_div.addEventListener("mouseleave", () => {
+      delete_btn.remove();
     });
   });
-
-  textInp.value = " ";
+  localStorage.setItem(date_inp.value, text_value);
+  text_inp.value = null;
+  date_inp.value = null;
 };
