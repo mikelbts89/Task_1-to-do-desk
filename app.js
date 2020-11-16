5424eb096e866386324b311740e6ecf1a968ef4f
mikelbts89 <mikelbts89@gmail.com>
2020-11-16 18:25:51 +0200

    new_start

--- app.js
+++ app.js
@@ -1,37 +1,91 @@
-let tasks_div = document.getElementById("tasks_div");
-let text_inp = document.getElementById("text_inp");
-let date_inp = document.getElementById("date_inp");
+let textContentArr = [];
+let dateContentArr = [];
+let textContent = document.getElementById("text_input");
+let dateContent = document.getElementById("date_input");
+let mainContentDiv = document.getElementById("main_content_div");
 
-const createTask = () => {
-  console.log(date_inp.value);
-  let main_div = document.createElement("div");
-  let paragraph = document.createElement("div");
-  let date_span = document.createElement("span");
-  date_span.innerText = date_inp.value;
-  paragraph.className = "main_parag";
-  main_div.className = "main_div";
-  let text_value = text_inp.value;
-  if (!text_value.trim()) {
-    return alert("No Task !");
+function refreshPage() {
+  location.reload();
+}
+
+function clearLocalStorage() {
+  localStorage.clear();
+}
+
+window.onload = () => {
+  if (localStorage.getItem("textContent") != null) {
+    textContentArr = JSON.parse(localStorage.getItem("textContent"));
+    dateContentArr = JSON.parse(localStorage.getItem("dateContent"));
   }
-  paragraph.innerText = text_value;
-  main_div.appendChild(paragraph);
-  main_div.appendChild(date_span);
-  tasks_div.appendChild(main_div);
+  drawAfterRefresh();
+};
 
-  main_div.addEventListener("mouseenter", () => {
-    let delete_btn = document.createElement("button");
-    delete_btn.className = "delete_btn";
-    delete_btn.innerText = "X";
-    main_div.appendChild(delete_btn);
-    delete_btn.addEventListener("click", (e) => {
+function getData() {
+  if (!textContent.value.trim()) {
+    return alert("No task!");
+  }
+  if (!dateContent.value) {
+    return alert("No date!");
+  }
+  let text = textContent.value.trim();
+  let date = dateContent.value;
+  textContentArr.push(text);
+  dateContentArr.push(date);
+  console.log(textContentArr, dateContentArr);
+  localStorage.setItem("textContent", JSON.stringify(textContentArr));
+  localStorage.setItem("dateContent", JSON.stringify(dateContentArr));
+  console.log(localStorage.getItem("textContent"));
+  console.log(localStorage.getItem("dateContent"));
+  draw();
+  textContent.value = "";
+  dateContent.value = "";
+}
+
+function draw() {
+  let cardDiv = document.createElement("div");
+  cardDiv.className = "card_div";
+  let textDiv = document.createElement("div");
+  textDiv.className = "text_div";
+  let dateDiv = document.createElement("div");
+  dateDiv.className = "date_div";
+  for (let i = 0; i < textContentArr.length; i++) {
+    textDiv.innerText = textContentArr[i];
+    dateDiv.innerText = dateContentArr[i];
+  }
+  cardDiv.appendChild(textDiv);
+  cardDiv.appendChild(dateDiv);
+  mainContentDiv.appendChild(cardDiv);
+  delElement(cardDiv);
+}
+
+function drawAfterRefresh() {
+  for (let i = 0; i < textContentArr.length; i++) {
+    let cardDiv = document.createElement("div");
+    cardDiv.className = "card_div";
+    let textDiv = document.createElement("div");
+    textDiv.className = "text_div";
+    let dateDiv = document.createElement("div");
+    dateDiv.className = "date_div";
+    textDiv.innerText = textContentArr[i];
+    dateDiv.innerText = dateContentArr[i];
+    cardDiv.appendChild(textDiv);
+    cardDiv.appendChild(dateDiv);
+    mainContentDiv.appendChild(cardDiv);
+    delElement(cardDiv);
+  }
+}
+
+function delElement(div) {
+  div.addEventListener("mouseenter", () => {
+    let delBtn = document.createElement("button");
+    delBtn.innerText = "X";
+    delBtn.className = "del_btn";
+    delBtn.addEventListener("click", (e) => {
       e.target.parentElement.remove();
     });
-    main_div.addEventListener("mouseleave", () => {
-      delete_btn.remove();
+    div.appendChild(delBtn);
+    div.addEventListener("mouseleave", () => {
+      delBtn.remove();
     });
   });
-  localStorage.setItem(date_inp.value, text_value);
-  text_inp.value = null;
-  date_inp.value = null;
-};
+}
