b010567cea4b71133353dbbe077c2a5f2510cae8
mikelbts89 <mikelbts89@gmail.com>
2020-11-19 18:32:15 +0200

    new_planing

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-let textContentArr = [];
-let dateContentArr = [];
+let taskDataArr = [];
 let textContent = document.getElementById("text_input");
 let dateContent = document.getElementById("date_input");
 let mainContentDiv = document.getElementById("main_content_div");
+
+window.onload = () => {
+  if (localStorage.getItem("taskData") != null) {
+    taskDataArr = JSON.parse(localStorage.getItem("taskData"));
+  }
+  draw(taskDataArr);
+};
 
 function refreshPage() {
   location.reload();
@@ -12,77 +18,25 @@ function clearLocalStorage() {
   localStorage.clear();
 }
 
-window.onload = () => {
-  if (localStorage.getItem("textContent") != null) {
-    textContentArr = JSON.parse(localStorage.getItem("textContent"));
-    dateContentArr = JSON.parse(localStorage.getItem("dateContent"));
-  }
-  drawAfterRefresh();
+let getTaskData = () => {
+  let taskData = {
+    task: textContent.value,
+    date: dateContent.value,
+    id: new Date(),
+  };
+  taskDataArr.push(taskData);
+  console.log(taskDataArr);
+  localStorage.setItem("taskData", JSON.stringify(taskDataArr));
+  draw(taskDataArr);
 };
 
-function getData() {
-  if (!textContent.value.trim()) {
-    return alert("No task!");
-  }
-  if (!dateContent.value) {
-    return alert("No date!");
-  }
-  let text = textContent.value.trim();
-  let date = dateContent.value;
-  textContentArr.push(text);
-  dateContentArr.push(date);
-  localStorage.setItem("textContent", JSON.stringify(textContentArr));
-  localStorage.setItem("dateContent", JSON.stringify(dateContentArr));
-  draw();
-  textContent.value = "";
-  dateContent.value = "";
-}
-
-function draw() {
-  let cardDiv = document.createElement("div");
-  cardDiv.className = "card_div";
-  let textDiv = document.createElement("div");
-  textDiv.className = "text_div";
-  let dateDiv = document.createElement("div");
-  dateDiv.className = "date_div";
-  for (let i = 0; i < textContentArr.length; i++) {
-    textDiv.innerText = textContentArr[i];
-    dateDiv.innerText = dateContentArr[i];
-  }
-  cardDiv.appendChild(textDiv);
-  cardDiv.appendChild(dateDiv);
-  mainContentDiv.appendChild(cardDiv);
-  delElement(cardDiv);
-}
-
-function drawAfterRefresh() {
-  for (let i = 0; i < textContentArr.length; i++) {
+let draw = () => {
+  mainContentDiv.innerHTML = "";
+  for (let i = 0; i < taskDataArr.length; i++) {
     let cardDiv = document.createElement("div");
     cardDiv.className = "card_div";
-    let textDiv = document.createElement("div");
-    textDiv.className = "text_div";
-    let dateDiv = document.createElement("div");
-    dateDiv.className = "date_div";
-    textDiv.innerText = textContentArr[i];
-    dateDiv.innerText = dateContentArr[i];
-    cardDiv.appendChild(textDiv);
-    cardDiv.appendChild(dateDiv);
+    cardDiv.innerText = taskDataArr[i].task;
     mainContentDiv.appendChild(cardDiv);
-    delElement(cardDiv);
+    console.log(taskDataArr[i]);
   }
-}
-
-function delElement(div) {
-  div.addEventListener("mouseenter", () => {
-    let delBtn = document.createElement("button");
-    delBtn.innerText = "✗";
-    delBtn.className = "del_btn";
-    delBtn.addEventListener("click", (e) => {
-      e.target.parentElement.remove();
-    });
-    div.appendChild(delBtn);
-    div.addEventListener("mouseleave", () => {
-      delBtn.remove();
-    });
-  });
-}
+};
