5b31bdd433929e9afe2a34ec8dabf23254a61253
mikelbts89 <mikelbts89@gmail.com>
2020-11-25 19:36:02 +0200

    adding_sone_code

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let taskDataArr = [];
 let textContent = document.getElementById("text_input");
 let dateContent = document.getElementById("date_input");
+let timeContent = document.getElementById("time_input");
 let mainContentDiv = document.getElementById("main_content_div");
 
 window.onload = () => {
@@ -19,9 +20,24 @@ function clearLocalStorage() {
 }
 
 let getTaskData = () => {
+  let currentDate = new Date();
+  if (!textContent.value) {
+    return alert("No task !!!");
+  }
+  if (
+    !dateContent.value ||
+    new Date(dateContent.value).getTime() <= currentDate.getTime()
+  ) {
+    return alert("The Date must be Bigger then today date !!!");
+  }
+  if (!timeContent.value) {
+    return alert("No time set !!!");
+  }
+
   let taskData = {
     task: textContent.value,
     date: dateContent.value,
+    time: timeContent.value,
     id: new Date(),
   };
   taskDataArr.push(taskData);
@@ -34,42 +50,48 @@ let draw = () => {
   for (let i = 0; i < taskDataArr.length; i++) {
     let cardDiv = document.createElement("div");
     let textDiv = document.createElement("div");
+    textDiv.className = "text_div";
     let dateDiv = document.createElement("div");
+    dateDiv.className = "date_div";
+    let timeDiv = document.createElement("div");
+    timeDiv.className = "time_div";
+    let cardId = taskDataArr[i].id;
     cardDiv.className = "card_div";
     textDiv.innerText = taskDataArr[i].task;
     dateDiv.innerText = taskDataArr[i].date;
+    timeDiv.innerText = taskDataArr[i].time;
     cardDiv.appendChild(textDiv);
     cardDiv.appendChild(dateDiv);
+    cardDiv.appendChild(timeDiv);
     mainContentDiv.appendChild(cardDiv);
-    delTask(cardDiv, taskDataArr[i].id);
-    finishedTask(cardDiv, taskDataArr[i].task, textDiv);
+    cardDiv.addEventListener("mouseenter", () => {
+      let delBtn = document.createElement("button");
+      delBtn.className = "del_btn";
+      delBtn.innerText = "❌";
+      delBtn.name = cardId;
+      cardDiv.appendChild(delBtn);
+      delBtn.addEventListener("click", (e) => {
+        e.target.parentElement.remove();
+        for (let i = 0; i < taskDataArr.length; i++) {
+          if (taskDataArr[i].id == delBtn.name) {
+            taskDataArr.splice(i, 1);
+            localStorage.setItem("taskData", JSON.stringify(taskDataArr));
+          }
+        }
+      });
+      let finishBtn = document.createElement("button");
+      finishBtn.className = "finished_btn";
+      finishBtn.innerText = "✔️";
+      cardDiv.appendChild(finishBtn);
+      finishBtn.onclick = (e) => {
+        e.target.parentElement.remove();
+      };
+      cardDiv.addEventListener("mouseleave", () => {
+        delBtn.remove();
+        finishBtn.remove();
+      });
+    });
   }
   textContent.value = "";
-};
-
-let delTask = (cDiv, buttonId) => {
-  let delBtn = document.createElement("button");
-  delBtn.className = "del_btn";
-  delBtn.innerText = "Delet Task";
-  delBtn.name = buttonId;
-  cDiv.appendChild(delBtn);
-  delBtn.addEventListener("click", (e) => {
-    e.target.parentElement.remove();
-    for (let i = 0; i < taskDataArr.length; i++) {
-      if (taskDataArr[i].id == delBtn.name) {
-        taskDataArr.splice(i, 1);
-        localStorage.setItem("taskData", JSON.stringify(taskDataArr));
-      }
-    }
-  });
-};
-
-let finishedTask = (cDiv, text, tDiv) => {
-  let finishBtn = document.createElement("button");
-  finishBtn.className = "del_btn";
-  finishBtn.innerText = "Finished Task";
-  cDiv.appendChild(finishBtn);
-  finishBtn.onclick = () => {
-    tDiv.innerHTML = text.strike();
-  };
+  dateContent.value = "";
 };
